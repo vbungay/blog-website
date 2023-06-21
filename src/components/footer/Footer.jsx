5849e1e759f6c7de737350a53a97287e21a86f6c
vbungay <113390913+vbungay@users.noreply.github.com>
2023-06-20 21:12:16 -0400

    basic styling

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './footer.module.scss'
+
+const Footer = () => {
+    return (
+        <div>
+            <div>
+                ©2023 Visual Network Creatives. All rights reserved.
+            </div>
+            <div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,61 @@
+"use client"
+
+import Link from 'next/link'
+import React from 'react'
+import styles from './navbar.module.scss'
+
+//array of navigation links
+const links = [
+    {
+        id: 1,
+        title: "Home",
+        url: "/",
+    },
+    {
+        id: 2,
+        title: "About",
+        url: "/about",
+    },
+    {
+        id: 3,
+        title: "Portfolio",
+        url: "/portfolio",
+    },
+    {
+        id: 4,
+        title: "Blog",
+        url: "/blog",
+    },
+    {
+        id: 5,
+        title: "Contact",
+        url: "/contact",
+    },
+]
+
+const Navbar = () => {
+    return (
+        <div className={styles.container}>
+            <Link href="/" className={styles.logo}>
+                visual network creatives
+            </Link>
+            <div className={styles.links}>
+                {links.map((link) => (
+                    <Link key={link.id} href={link.url} className={styles.link}>
+                        {link.title}
+                    </Link>
+                ))}
+                <button
+                    className={styles.btn}
+                    onClick={() => {
+                        console.log("logged out")
+                    }}
+                >
+                    Logout
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
