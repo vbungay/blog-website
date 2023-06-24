0b4c8eacf88d59879b355dac622357c8e1d98bb9
vbungay <113390913+vbungay@users.noreply.github.com>
2023-06-23 22:23:59 -0400

    ,

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -25,10 +25,16 @@ const About = () => {
             Welcome to Visual Network Creatives, where creativity meets connectivity! We are a dynamic web design brand dedicated
             to crafting visually stunning and highly functional digital experiences that leave a lasting impact on your audience.
             <br />
+            <br />
             At Visual Network Creatives, we believe that a successful website goes beyond aesthetics. It should be intuitive,
             user-friendly, and optimized for performance. We leverage the latest web technologies and industry best practices to
             create fast-loading, SEO-friendly websites that engage users and drive conversions. Whether you need an e-commerce site,
             a corporate portfolio, or a cutting-edge web application, we have the expertise to bring your vision to life.
+            <br />
+            <br />
+            We take pride in our commitment to client satisfaction and believe in building long-term partnerships. Throughout the design 
+            process, we maintain open lines of communication, ensuring that your input and feedback are incorporated at every stage.
+            <br />
             <br />
             Embrace the power of visual connectivity with Visual Network Creatives. Let us transform your online presence into a 
             captivating network of creativity, functionality, and success. Contact us today to embark on a journey towards a 
@@ -37,23 +43,27 @@ const About = () => {
         </div>
         <div className={styles.item}>
           <h1 className={styles.title}>Our Services</h1>
-          <p>
+          <p className={styles.desc}>
               1. Custom Web Design: We create visually stunning websites that reflect your brand's personality, ensuring a captivating 
               online presence that sets you apart from the competition.
+              <br />
               <br />
               2. Responsive Web Development: We build websites that adapt seamlessly to different devices and screen sizes, providing an 
               optimal user experience across desktops, tablets, and mobile devices.
               <br />
+              <br />
               3. Search Engine Optimization (SEO): We optimize your website's structure, content, and performance to improve visibility 
               on search engines, driving organic traffic and boosting your online presence.
+              <br />
               <br />
               4. Social Media Integration: We seamlessly integrate your website with various social media platforms, enabling easy sharing, 
               engagement, and amplification of your brand across social channels.
               <br />
+              <br />
               5. Web Maintenance and Support: We provide ongoing website maintenance and support services, ensuring your website remains secure, 
               up-to-date, and optimized for performance.
           </p>
-          <Button url="/contact" text="Get in touch" />
+          <Button url="/contact" text="Request a quote" />
         </div>
       </div>
     </div>
